079f85e6abf3c00ce5c0f177e5d88e020eacbb97
ayushporwal01 <porwalayush953@gmail.com>
2026-05-10 10:03:42 +0530

    Initial commit

--- 01 - Basics/Patterns/patterns.cpp
+++ 01 - Basics/Patterns/patterns.cpp
@@ -2,46 +2,58 @@
 
 using namespace std;
 
-//Square 
-void print1(int n) {
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+// Square
+void print1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cout << "*";
         }
         cout << endl;
     }
 }
 
-//Stairs
-void print2(int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
+// Stairs
+void print2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             cout << "*";
         }
         cout << endl;
     }
 }
 
-/* 1 
+/* 1
    1 2
    1 2 3
 */
-void print3(int n) {
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= i; j++) {
+void print3(int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cout << j;
         }
         cout << endl;
     }
 }
 
-/* 1 
+/* 1
    2 2
    3 3 3
 */
-void print4(int n) {
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= i; j++) {
+void print4(int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cout << i;
         }
         cout << endl;
@@ -50,124 +62,158 @@ void print4(int n) {
 
 /* 5 stars
    4 stars
-   3 stars 
+   3 stars
    2 stars
    1 star
 */
-void print5(int n) {
-    for (int i = n; i >= 0; i--) {
-        for (int j = i; j >= 0; j--) {
+void print5(int n)
+{
+    for (int i = n; i >= 0; i--)
+    {
+        for (int j = i; j >= 0; j--)
+        {
             cout << "*";
         }
         cout << endl;
     }
 }
 
-/* 1 2 3 
+/* 1 2 3
    1 2
    1
 */
-void print6(int n) {
-    for (int i = n; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) {
+void print6(int n)
+{
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cout << j;
         }
         cout << endl;
     }
 }
 
-//Pyramid
-void print7(int n) {
-    for (int i = 0; i < n; i++) {
+// Pyramid
+void print7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         // space
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
         // star
-        for (int j = 0; j < 2 * i + 1; j++) {
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
             cout << "*";
         }
         // space
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
         cout << endl;
     }
 }
 
-//Reverse Pyramid
-void print8(int n) { 
-    for (int i = n; i > 0; i--) { //n = 4 -> 1
+// Reverse Pyramid
+void print8(int n)
+{
+    for (int i = n; i > 0; i--)
+    { // n = 4 -> 1
         // space
-        for (int j = 0; j < n - i; j++) { 
+        for (int j = 0; j < n - i; j++)
+        {
             cout << " ";
         }
         // star
-        for (int j = 0; j < 2 * i - 1; j++) {
+        for (int j = 0; j < 2 * i - 1; j++)
+        {
             cout << "*";
-        }   
+        }
         // space
-        for (int j = 0; j < n - i; j++) {
+        for (int j = 0; j < n - i; j++)
+        {
             cout << " ";
         }
         cout << endl;
     }
 }
 
-//Diamond
-void print9(int n) {
-    for (int i = 0; i < n; i++) {
+// Diamond
+void print9(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         // space
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
         // star
-        for (int j = 0; j < 2 * i + 1; j++) {
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
             cout << "*";
         }
         // space
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
         cout << endl;
     }
 
-    for (int i = n; i > 0; i--) {
+    for (int i = n; i > 0; i--)
+    {
         // space
-        for (int j = 0; j < n - i; j++) {
+        for (int j = 0; j < n - i; j++)
+        {
             cout << " ";
-        }   
+        }
         // star
-        for (int j = 0; j < 2 * i - 1; j++) {
+        for (int j = 0; j < 2 * i - 1; j++)
+        {
             cout << "*";
         }
         // space
-        for (int j = 0; j < n - i; j++) {
+        for (int j = 0; j < n - i; j++)
+        {
             cout << " ";
         }
         cout << endl;
     }
 }
 
-//Butterfly
-void print10(int n) {
-    for (int i = 1; i <= 2 * n - 1; i++) {
+// Right Half Butterfly
+void print10(int n)
+{
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
         int stars = i;
-        if(i > n) stars = 2 * n - i; 
-       
-        for (int j = 1; j <= stars; j++) {
+        if (i > n)
+            stars = 2 * n - i;
+
+        for (int j = 1; j <= stars; j++)
+        {
             cout << "*";
         }
         cout << endl;
     }
 }
 
-void print11(int n) {
+void print11(int n)
+{
     int start = 1;
-    for (int i = 0; i <= n; i++) {
-        if(i % 2 == 0) start = 1;
-        else start = 0;
-        for(int j = 0; j <= i; j++) {
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start = 0;
+        for (int j = 0; j <= i; j++)
+        {
             cout << start;
             start = 1 - start;
         }
@@ -175,21 +221,26 @@ void print11(int n) {
     }
 }
 
-void print12(int n) {
+void print12(int n)
+{
     int space = 2 * (n - 1);
-    for(int i = 1; i <= n; i++) {
-        //numbers
-        for(int j = 1; j <= i; j++) {
+    for (int i = 1; i <= n; i++)
+    {
+        // numbers
+        for (int j = 1; j <= i; j++)
+        {
             cout << j;
         }
 
-        //spaces
-        for(int j = 1; j <= space; j++) {
+        // spaces
+        for (int j = 1; j <= space; j++)
+        {
             cout << " ";
         }
 
-        //numbers
-        for(int j = i; j >= 1; j--) {
+        // numbers
+        for (int j = i; j >= 1; j--)
+        {
             cout << j;
         }
         cout << endl;
@@ -197,10 +248,13 @@ void print12(int n) {
     }
 }
 
-void print13(int n) {
+void print13(int n)
+{
     int num = 1;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= i; j++) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cout << num << " ";
             num = num + 1;
         }
@@ -208,160 +262,206 @@ void print13(int n) {
     }
 }
 
-void print14(int n) {
-    for(int i = 0; i < n; i++) {
-        for(char ch = 'A'; ch <= 'A' + i; ch++) {
+void print14(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + i; ch++)
+        {
             cout << ch << " ";
         }
         cout << endl;
     }
 }
 
-void print15(int n) {
-    for(int i = 0; i < n; i++) {
-        for(char ch = 'A'; ch <= 'A' + (n - i - 1); ch++) {
+void print15(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++)
+        {
             cout << ch << " ";
         }
         cout << endl;
     }
 }
 
-void print16(int n) {
-    for(int i = 0; i < n; i++) {
+void print16(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         char ch = 'A' + i;
-        for(int j = 0; j <= i; j++) {
+        for (int j = 0; j <= i; j++)
+        {
             cout << ch << " ";
         }
         cout << endl;
     }
 }
 
-void print17(int n) {
-    for(int i = 0; i < n; i++) {
-        //spaces
-        for(int j = 0; j < n - i - 1; j++) {
+void print17(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // spaces
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
 
-        //characters
+        // characters
         char ch = 'A';
         int breakpoint = (2 * i + 1) / 2;
-        for(int j = 1; j <= 2 * i + 1; j++) {
+        for (int j = 1; j <= 2 * i + 1; j++)
+        {
             cout << ch;
-            if(j <= breakpoint) ch++;
-            else ch--;
+            if (j <= breakpoint)
+                ch++;
+            else
+                ch--;
         }
 
-        //spaces
-        for(int j = 0; j < n - i - 1; j++) {
+        // spaces
+        for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
         }
         cout << endl;
     }
 }
 
-void print18(int n) {
-    for(int i = 0; i < n; i++) {
-        for(char ch = 'E' - i; ch <= 'E'; ch++) {
+void print18(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
             cout << ch << " ";
-        } 
-        cout << endl;
-    }
-}
-
-void print19(int n) {
-    //Upper Half
-    for(int i = 0; i < n; i++) {
-        //stars
-        for(int j = 1; j <= n - i; j++) {
-            cout << "*";
-        }
-
-        //space
-        for(int j = 0; j < 2 * i; j++) {
-            cout << " ";
-        }
-
-        //stars
-        for(int j = 1; j <= n - i; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    //Lower Half
-    for(int i = 0; i < n; i++) {
-        //stars
-        for(int j = 0; j <= i; j++) {
-            cout << "*";
-        }
-
-        //space
-        for(int j = 0; j < 2 * (n - i - 1); j++) {
-            cout << " ";
-        }
-
-        //stars
-        for(int j = 0; j <= i; j++) {
-            cout << "*";
         }
         cout << endl;
     }
 }
 
-void print20(int n) {   
-    int spaces = 2 * n - 2; 
-    for(int i = 1; i <= 2 * n - 1; i++) {
+void print19(int n)
+{
+    // Upper Half
+    for (int i = 0; i < n; i++)
+    {
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+
+        // space
+        for (int j = 0; j < 2 * i; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Lower Half
+    for (int i = 0; i < n; i++)
+    {
+        // stars
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        // space
+        for (int j = 0; j < 2 * (n - i - 1); j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void print20(int n)
+{
+    int spaces = 2 * n - 2;
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
         int stars = i;
-        if(i > n) stars = 2 * n - i;
+        if (i > n)
+            stars = 2 * n - i;
 
-        //stars
-        for(int j = 1; j <= stars; j++) {
+        // stars
+        for (int j = 1; j <= stars; j++)
+        {
             cout << "*";
         }
 
-        //space
-        for(int j = 1; j <= spaces; j++) {
+        // space
+        for (int j = 1; j <= spaces; j++)
+        {
             cout << " ";
         }
 
-        //stars
-        for(int j = 1; j <= stars; j++) {
+        // stars
+        for (int j = 1; j <= stars; j++)
+        {
             cout << "*";
         }
         cout << endl;
-        if(i < n) spaces -= 2;
-        else spaces += 2;
+        if (i < n)
+            spaces -= 2;
+        else
+            spaces += 2;
     }
 }
 
-void print21(int n) {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+void print21(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
                 cout << "*";
-            } else {
+            }
+            else
+            {
                 cout << " ";
             }
         }
-        cout << endl;  
+        cout << endl;
     }
 }
 
-void print22(int n) {
-    for(int i = 0; i < 2 * n - 1; i++) {
-        for(int j = 0; j < 2*n - 1; j++) {
+void print22(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
             int top = i;
             int left = j;
-            int right = (2*n - 2) - i;
-            int bottom = (2*n - 2) - j;
+            int right = (2 * n - 2) - i;
+            int bottom = (2 * n - 2) - j;
             cout << (n - min(min(top, bottom), min(left, right)));
         }
-        cout << endl;  
+        cout << endl;
     }
 }
 
-int main() {
+int main()
+{
     // print1(4);
     // print2(5);
     // print3(5);
@@ -371,7 +471,7 @@ int main() {
     // print7(5);
     // print8(5);
     // print9(5);
-    print10(5);
+    // print10(5);
     // print11(4);
     // print12(4);
     // print13(4);
