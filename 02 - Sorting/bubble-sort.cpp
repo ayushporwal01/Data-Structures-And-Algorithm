45db37f53d28edaac75bed3107d655046ff59890
ayushporwal01 <porwalayush953@gmail.com>
2026-05-11 22:23:34 +0530

    Initial commit

--- 02 - Sorting/bubble-sort.cpp
+++ 02 - Sorting/bubble-sort.cpp
@@ -6,11 +6,12 @@ using namespace std;
 void bubbleSort(int arr[], int n) {
   for(int i = 0; i < n - 1; i++) {
     bool didSwap = false;
-    for(int j = 0; j <= i - 1; j++) {
+    for(int j = 0; j < n - i - 1; j++) {
       if(arr[j] > arr[j + 1]) {
-        int temp = arr[j + 1];
-        arr[j + 1] =  arr[j];
-        arr[j] = temp;
+        int temp = arr[j];
+        arr[j] =  arr[j + 1];
+        arr[j + 1] = temp;
+
         didSwap = true;
       }
     }
