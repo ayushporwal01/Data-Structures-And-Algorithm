2d14198659b7371ac7e897cc6ddff0494d41b540
ayushporwal01 <porwalayush953@gmail.com>
2026-05-11 22:20:13 +0530

    Initial commit

--- 02 - Sorting/bubble-sort.cpp
+++ 02 - Sorting/bubble-sort.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 //Time Complexity - Worst & Avg: O(N^2), Best: O(N)
 void bubbleSort(int arr[], int n) {
-    for(int i = n - 1; i >= 0; i--) {
-      int didSwap = 0;
-      for(int j = 0; j <= i - 1; j++) {
-        if(arr[j] > arr[j + 1]) {
-          int temp = arr[j + 1];
-          arr[j + 1] =  arr[j];
-          arr[j] = temp;
-          didSwap = 1;
-        }
-      }
-      if(didSwap == 0) {
-        break;
+  for(int i = 0; i < n - 1; i++) {
+    bool didSwap = false;
+    for(int j = 0; j <= i - 1; j++) {
+      if(arr[j] > arr[j + 1]) {
+        int temp = arr[j + 1];
+        arr[j + 1] =  arr[j];
+        arr[j] = temp;
+        didSwap = true;
       }
     }
+    if(!didSwap) {
+      break;
+    }
+  }
 }
 
 int main() {
