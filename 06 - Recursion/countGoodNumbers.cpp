3b4534c31f3f865bcfeaf5f6ce0961b1389375f3
ayushporwal01 <porwalayush953@gmail.com>
2026-05-17 10:24:49 +0530

    Initial commit

--- 06 - Recursion/countGoodNumbers.cpp
+++ 06 - Recursion/countGoodNumbers.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//Binary Exponentiation - TC: O(logN), SC: O(logN)
+//Brute - TC: O(logN), SC: O(logN) - Binary Exponentiation
 const int M = 1e9 + 7;
 int findPower(long long a, long long b) {
    if(b == 0) return 1;
@@ -19,6 +19,29 @@ int findPower(long long a, long long b) {
 
 int countGoodNumbers(long long n) {
    return (long long)findPower(5, (n+1)/2) * findPower(4, n/2) % M;
+}
+
+//Optimal
+int findPower2(long long x, long long n) {
+   long long res = 1;
+
+   while(n > 0) {
+      if(n % 2 == 1) {
+         res = (res % x) % M;
+      }
+
+      x = (x * x) % M;
+      n /= 2;
+   }
+
+   return res;
+}
+
+int countGoodNumbers(long long n) {
+   long long odd = (n + 1) / 2;
+   long long even = n / 2;
+
+   return (findPower2(5, odd) * findPower2(4, even)) % M;
 }
 
 int main() {
