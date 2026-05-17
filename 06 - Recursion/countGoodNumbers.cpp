9e0ad427d6affb098bc3f807089ec04893fa77e5
ayushporwal01 <porwalayush953@gmail.com>
2026-05-17 10:28:42 +0530

    Initial commit

--- 06 - Recursion/countGoodNumbers.cpp
+++ 06 - Recursion/countGoodNumbers.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//Brute - TC: O(logN), SC: O(logN) - Recursive(Binary Expo)
+//Brute - TC: O(logN), SC: O(logN) - Recursive Binary Expo
 const int M = 1e9 + 7;
 int findPower(long long a, long long b) {
    if(b == 0) return 1;
@@ -21,7 +21,7 @@ int countGoodNumbers(long long n) {
    return (long long)findPower(5, (n+1)/2) * findPower(4, n/2) % M;
 }
 
-//Optimal(TC: O(logN), SC:O(1)) - Iterative
+//Optimal - TC: O(logN), SC:O(1) - Iterative
 int findPower2(long long x, long long n) {
    long long res = 1;
 
